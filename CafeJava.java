5975a1b5450c893971ec6c3e32410eaa58180d22
Brian Denmark <testingarea479@gmail.com>
2022-12-05 10:36:34 -0800

    init commit

---
+++ CafeJava.java
@@ -0,0 +1,22 @@
+public class CafeJava {
+    public static void main(String[] args) {
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly.";
+        String readyMessage = ", your order is ready!";
+        String displayTotalMessage = "Your total is $";
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+
+        // Customer name variables
+        String customer1 = "Cindhuri";
+
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+
+        // App interaction simulation (add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1);
+
+        // Customer interaction print statements here
+    }
+}   
