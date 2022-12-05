0f3a050d264f31d1ed87c80ebd37c5ef575d75a3
Brian Denmark <testingarea479@gmail.com>
2022-12-05 10:47:52 -0800

    added sim for customer4

--- CafeJava.java
+++ CafeJava.java
@@ -26,6 +26,12 @@ public class CafeJava {
         // Example:
         System.out.println(generalGreeting + customer1);
         System.out.println(customer1 + readyMessage + displayTotalMessage + dripCoffeePrice);
+        
+        if(isReadyOrder4){
+            System.out.println(customer4 + readyMessage + displayTotalMessage + cappucinoPrice);
+        } else {
+            System.out.println(customer4 + pendingMessage);
+        }
 
         // Customer interaction print statements here
     }
