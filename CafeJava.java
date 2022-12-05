f7b8757506d51297574336cbe0786b5ce56a7d0f
Brian Denmark <testingarea479@gmail.com>
2022-12-05 10:52:53 -0800

    added sim for customer2

--- CafeJava.java
+++ CafeJava.java
@@ -25,14 +25,21 @@ public class CafeJava {
         // App interaction simulation (add your code for the challenges below)
         // Example:
         System.out.println(generalGreeting + customer1);
-        System.out.println(customer1 + readyMessage + displayTotalMessage + dripCoffeePrice);
         
+        System.out.println(customer1 + readyMessage + displayTotalMessage + dripCoffeePrice);
+
         if(isReadyOrder4){
             System.out.println(customer4 + readyMessage + displayTotalMessage + cappucinoPrice);
         } else {
             System.out.println(customer4 + pendingMessage);
         }
 
+        isReadyOrder2 = true;
+        if(isReadyOrder2){
+            System.out.println(customer2 + readyMessage + displayTotalMessage + (lattePrice*2));
+        } else {
+            System.out.println(customer2 + pendingMessage);
+        }
         // Customer interaction print statements here
     }
 }   
